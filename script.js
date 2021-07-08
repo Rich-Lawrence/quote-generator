4e2d7eda902e3158e9b60351a67c17cfade5d45b
Rich Lawrence <Richie.l.lawrence@gmail.com>
2021-07-08 17:23:05 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn= document.getElementById('new-quote');
+
+
+let apiQuotes = [];
+//Show new quote
+
+function newQuote() {
+    //Pick a random quote
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    // Check if author field is blank and if so replace with 'Unknown'
+    if(!quote.author){
+      authorText.textContent = "Unknown";
+    }else{
+      authorText.textContent = quote.author;
+    }
+    //Check quote length to determine styling
+    if(quote.text.length > 120){
+      quoteText.classList.add('long-quote');
+    }else{
+      quoteText.classList.remove('long-quote');
+    }
+      quoteText.textContent = quote.text;
+};
+
+// Get quotes from API
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try{
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      newQuote();
+    } catch (error){
+      //catch error here
+    }
+}
+
+//onload
+
+getQuotes()
