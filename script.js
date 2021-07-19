7dffde12306d81cab4292f6db648066a4e8c19ce
Rich Lawrence <Richie.l.lawrence@gmail.com>
2021-07-19 15:47:37 +0100

    added loader

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn= document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
+
+// Show Loading
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 //Show new quote
 
 function newQuote() {
+    loading();
     //Pick a random quote
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
@@ -24,12 +41,15 @@ function newQuote() {
     }else{
       quoteText.classList.remove('long-quote');
     }
+      // Set Quote, Hide Loader
       quoteText.textContent = quote.text;
+      complete();
 };
 
 // Get quotes from API
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
       const response = await fetch(apiUrl);
